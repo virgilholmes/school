97dd7e3dd8a049348ad0eef69daa2c952c832ad6
Virgil Holmes <me@virgilholmes.com>
2026-03-05 07:56:19 -0500

    uploaded real code.
    Added a function to continuously rotate a servo motor.

--- 2025-2026/overflow/main.py
+++ 2025-2026/overflow/main.py
@@ -1,1 +1,9 @@
-# overflow project code - virgilholmes 02/10/2026
+# overflow project code - virgilholmes 
+def on_forever():
+  for i in range(181): #spinny thing
+    pins.servo_write_pin(AnalogPin.P0,i)
+    basic.pause(40)
+  pins.servo_write_pin(AnalogPin.P0,0)
+  basic.pause(1000)
+basic.forever(on_forever)
+
